0934f879c57a57c4d24ef9c2d0a111ee661f7801
Everaldo Lima <jemlima@gmail.com>
2019-06-28 18:21:37 -0300

    Adicionado novas funcionalidades ao projeto.

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ module.exports = {
   dialect: 'mysql',
   host: 'localhost',
   username: 'root',
-  password: '',
+  password: 'root',
   database: 'gobarber',
   define: {
     timestamps: true,
--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,17 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Everaldo Lima',
-    email: 'jemlima@gmail.com',
-    password_hash: '321354687987984',
-  });
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
 
 export default routes;
