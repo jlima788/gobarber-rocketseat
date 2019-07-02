c5f0e66f21fa871c44b37f55e43f86b4b291caf1
Everaldo Lima <jemlima@gmail.com>
2019-07-02 16:57:19 -0300

    Incluindo listagem de provedores.

--- src/config/database.js
+++ src/config/database.js
@@ -2,11 +2,11 @@ module.exports = {
   dialect: 'mysql',
   host: 'localhost',
   username: 'root',
-  password: '',
+  password: 'root',
   database: 'gobarber',
   define: {
     timestamps: true,
     underscored: true,
-    underscoredAll: true,
-  },
-};
+    underscoredAll: true
+  }
+}
