11cfae0e909695b05b279af1f3f207d6a25f676c
Everaldo <everaldo@agenciamanga.com.br>
2019-07-09 22:24:05 -0300

    Mandando e-mail de cancelamento

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '6a759792da9cef',
+    pass: '8f76122e37a682',
+  },
+  default: {
+    from: 'Equeipe GoBarber <noreply@gobarber.com>',
+  },
+};
